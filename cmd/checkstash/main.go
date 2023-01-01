bb0ea977eda8b0172115694b65ef0dac62c75cf2
S0me0neR0man <roman.zitr@yandex.ru>
2023-01-02 01:18:10 +0300

    grpc insert, get

--- cmd/checkstash/main.go
+++ cmd/checkstash/main.go
@@ -4,26 +4,65 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"os/signal"
+	"strconv"
 	"sync"
 	"syscall"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/anypb"
 
 	"ourstash/internal/grpcproto"
+	"ourstash/internal/stashdb"
 )
 
 const (
-	insertInterval = time.Millisecond * 10
+	insertInterval = time.Millisecond * 100
 )
 
 type oneRecord struct {
-	guid string
-	data map[string]any
-
+	section stashdb.SectionIdType
+	guid    stashdb.GUIDType
+	data    map[string]*anypb.Any
 	deleted bool
+}
+
+func newOneRecord() oneRecord {
+	i := rand.Intn(100)
+	rec := oneRecord{
+		section: stashdb.SectionIdType(i + 1),
+		deleted: false,
+		data:    make(map[string]*anypb.Any),
+	}
+
+	val, err := anypb.New(&grpcproto.StringData{
+		Data: "#tag" + strconv.Itoa(i),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	rec.data["tag"] = val
+
+	val, err = anypb.New(&grpcproto.StringData{
+		Data: "sample text" + strconv.Itoa(i),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	rec.data["text"] = val
+
+	val, err = anypb.New(&grpcproto.IntData{
+		Data: int64(i),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	rec.data["int_value"+strconv.Itoa(i)] = val
+
+	return rec
 }
 
 func main() {
@@ -38,8 +77,11 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 	defer stop()
 
+	toGet := make(chan oneRecord, 100)
+
 	var wg sync.WaitGroup
-	wg.Add(1)
+	wg.Add(2)
+
 	go func(ctx context.Context) {
 		defer wg.Done()
 		for {
@@ -47,26 +89,75 @@ func main() {
 			case <-ctx.Done():
 				return
 			default:
-				log.Print(".")
+				rec := newOneRecord()
+				rec.guid, err = insert(ctx, c, rec)
+				if err != nil {
+					log.Println(err)
+					time.Sleep(insertInterval)
+					continue
+				}
+				toGet <- rec
 				time.Sleep(insertInterval)
 			}
 		}
 	}(ctx)
 
-	insert(c)
+	go func(ctx context.Context) {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case rec := <-toGet:
+				after, err := get(ctx, c, rec)
+				if err != nil && !rec.deleted {
+					log.Println(err)
+					continue
+				}
+				if rec.guid != after.guid {
+					log.Printf("get error guid before=%s after=%s", rec.guid, after.guid)
+				}
+				for field, val := range rec.data {
+					if val.MessageIs(after.data[field]) {
+						log.Printf("get error '%s' before=%v after=%v", field, val, after.data[field])
+					}
+				}
+			}
+		}
+	}(ctx)
+
 	wg.Wait()
 }
 
-func insert(c grpcproto.StashClient) {
-	resp, err := c.Get(context.Background(), &grpcproto.GetRequest{
-		Section: 0,
-		Guid:    "test",
+func get(ctx context.Context, c grpcproto.StashClient, rec oneRecord) (oneRecord, error) {
+	resp, err := c.Get(ctx, &grpcproto.GetRequest{
+		Section: uint32(rec.section),
+		Guid:    string(rec.guid),
 	})
+
 	if err != nil {
-		log.Fatal(err)
+		return oneRecord{}, fmt.Errorf("c.Get: %w", err)
 	}
 	if resp.Error != "" {
-		fmt.Println(resp.Error)
+		return oneRecord{}, fmt.Errorf("resp.Error: %s", resp.Error)
 	}
-	log.Println(resp.Data)
+
+	rec.data = resp.Data
+	return rec, nil
+}
+
+func insert(ctx context.Context, c grpcproto.StashClient, rec oneRecord) (stashdb.GUIDType, error) {
+	resp, err := c.Insert(ctx, &grpcproto.InsertRequest{
+		Section: uint32(rec.section),
+		Data:    rec.data,
+	})
+
+	if err != nil {
+		return "", err
+	}
+	if resp.Error != "" {
+		return "", fmt.Errorf("resp.Error: %s", resp.Error)
+	}
+
+	return stashdb.GUIDType(resp.Guid), nil
 }
--- internal/stashserver/grpcserver.go
+++ internal/stashserver/grpcserver.go
@@ -2,10 +2,12 @@ package stashserver
 
 import (
 	"context"
+	"fmt"
 	"net"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/anypb"
 
 	"ourstash/internal/grpcproto"
 	"ourstash/internal/stashdb"
@@ -46,12 +48,77 @@ func (ss *StashServer) GracefulStop() {
 func (ss *StashServer) Insert(ctx context.Context, in *grpcproto.InsertRequest) (*grpcproto.InsertResponse, error) {
 	var resp grpcproto.InsertResponse
 
+	section, err := ss.getSection(in.Section)
+	if err != nil {
+		resp.Error = err.Error()
+		return &resp, err
+	}
+
+	data := make(map[string]any)
+	for field, val := range in.Data {
+		switch val.GetTypeUrl() {
+		case "type.googleapis.com/grpcs.IntData":
+			intData := &grpcproto.IntData{}
+			if err := val.UnmarshalTo(intData); err != nil {
+				resp.Error = err.Error()
+				return &resp, err
+			}
+			data[field] = intData.GetData()
+		case "type.googleapis.com/grpcs.StringData":
+			strData := &grpcproto.StringData{}
+			if err := val.UnmarshalTo(strData); err != nil {
+				resp.Error = err.Error()
+				return &resp, err
+			}
+			data[field] = strData.GetData()
+		default:
+			ss.sugar.Errorw("unknown", "TypeUrl", val.GetTypeUrl())
+		}
+	}
+	resp.Guid = string(ss.stash.Insert(section, data))
+
 	return &resp, nil
 }
 
 func (ss *StashServer) Get(ctx context.Context, in *grpcproto.GetRequest) (*grpcproto.GetResponse, error) {
 	var resp grpcproto.GetResponse
 
+	section, err := ss.getSection(in.Section)
+	if err != nil {
+		resp.Error = err.Error()
+		return &resp, err
+	}
+
+	var data map[string]any
+	data, err = ss.stash.Get(section, stashdb.GUIDType(in.GetGuid()))
+	if err != nil {
+		resp.Error = err.Error()
+		return &resp, err
+	}
+
+	resp.Data = make(map[string]*anypb.Any)
+	for key, val := range data {
+		switch val.(type) {
+		case int64:
+			a, err := anypb.New(&grpcproto.IntData{
+				Data: val.(int64),
+			})
+			if err != nil {
+				resp.Error = err.Error()
+				return &resp, err
+			}
+			resp.Data[key] = a
+		case string:
+			a, err := anypb.New(&grpcproto.StringData{
+				Data: val.(string),
+			})
+			if err != nil {
+				resp.Error = err.Error()
+				return &resp, err
+			}
+			resp.Data[key] = a
+		}
+	}
 	return &resp, nil
 }
 
@@ -65,4 +132,11 @@ func (ss *StashServer) Remove(ctx context.Context, in *grpcproto.RemoveRequest) 
 	var resp grpcproto.RemoveResponse
 
 	return &resp, nil
+}
+
+func (ss *StashServer) getSection(in uint32) (stashdb.SectionIdType, error) {
+	if in > 254 {
+		return 0xff, fmt.Errorf("section must be < 255")
+	}
+	return stashdb.SectionIdType(in), nil
 }
